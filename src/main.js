e607a7b34528e8a9e91f9604e1f9b09d35e96561
Somto Zikora <somto@polargold.de>
2019-11-25 12:49:34 +0100

    FOO

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
+import vueCustomElement from 'vue-custom-element'
 import App from './App.vue'
 
-Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+Vue.config.productionTip = false
+Vue.use(vueCustomElement)
+// new Vue({
+//   render: h => h(App),
+// }).$mount('#app')
+
+Vue.customElement('vue-wid', App)
